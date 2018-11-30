92aae4f420e0323fc02294d8204f43180a68d3e7
ben stear <bjs385@drexel.edu>
2018-11-30 16:07:56 -0500

    Add files via upload

---
+++ scripts/plots.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 16:01:32 2018
+
+@author: dawnstear
+"""
+
+import numpy as np
+import pandas as pd
+
+
+# plotting functions for different types of data
+
+def plotcont():
+    pass
+def plotdisc():
+    pass
+def plotbinary():
+    pass
