62e78511802f29c1d4f9e13bdb1496f9b802c694
ben stear <bjs385@drexel.edu>
2018-12-10 11:23:16 -0500

    Add files via upload

---
+++ test_scripts/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 13:55:08 2018
+
+@author: dawnstear
+"""
+
+import numpy as np
+#from videowidget import videowidget as vw# import display_video, getframe
+import utilities as u
+from mainwindow import MainWindow
+
+#l = u.load('/Users/dawnstear/desktop/scGUI/fake_data.csv')
+#u.create_db()
+'''
+vidfile = '/Users/dawnstear/desktop/GOPR1457.MP4' 
+vw.display_video(vidfile)
+currentframe = vw.getframe()
+print(currentframe)
+'''
+
+MainWindow()
+'''
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
+'''
